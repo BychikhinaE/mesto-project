f041b7d957eb08550bbeaf8b494db8ec1be51bd2
Max Tishenko <rd-max@ya.ru>
2022-06-16 19:39:08 +0300

    fix card

--- src/components/card.js
+++ src/components/card.js
@@ -1,90 +1,99 @@
 // функции для работы с карточками проекта Mesto
 //объявить функию для создания карточки
-import {openPopup} from '../utils/utils';
-import {profileName,} from './modal.js'
-import { plusLike, disLike} from './Api'
+import { openPopup } from "../utils/utils";
+import { profileName } from "./modal.js";
+import { plusLike, disLike } from "./Api";
 
-export const popupPlace = document.querySelector('.popup_type_place');
-export const popupQuestionDelete = document.querySelector('.popup_type_delete');
-export const buttonQuestionDelete = popupQuestionDelete.querySelector('#questionDelete');
+export const popupPlace = document.querySelector(".popup_type_place");
+export const popupQuestionDelete = document.querySelector(".popup_type_delete");
+export const buttonQuestionDelete =
+  popupQuestionDelete.querySelector("#questionDelete");
 export const formPlace = document.forms.formPlace;
-export const buttonCreatePlace = formPlace.querySelector('.popup__button');
-const cardsContainer = document.querySelector('.elements');
-const fullPhoto = document.querySelector('.popup_type_photo');
-const popupPhoto = fullPhoto.querySelector('.popup__photo');
-const subtitlePhoto = fullPhoto.querySelector('.popup__subtitle');
-const buttonAdd = document.querySelector('.profile__add');
+export const buttonCreatePlace = formPlace.querySelector(".popup__button");
+const cardsContainer = document.querySelector(".elements");
+const fullPhoto = document.querySelector(".popup_type_photo");
+const popupPhoto = fullPhoto.querySelector(".popup__photo");
+const subtitlePhoto = fullPhoto.querySelector(".popup__subtitle");
+const buttonAdd = document.querySelector(".profile__add");
 //const cardTemplate = document.querySelector('#card-template').content;
 export let cardIdDelete = 0;
 
- //добавить кнопкe Плюс_карточка функцию Открыть попап
- buttonAdd.addEventListener('click', function () {
+//добавить кнопкe Плюс_карточка функцию Открыть попап
+buttonAdd.addEventListener("click", function () {
   openPopup(popupPlace);
 });
 
 //Функция создания новой карточки
+//123
 class Card {
-  constructor({nameValue, urlValue, countLike, ownerId, card_id}, templateSelector){
-    const cardElement = document.querySelector(templateSelector).content.querySelector('.elements__card').cloneNode(true);
+  constructor(
+    { nameValue, urlValue, countLike, ownerId, card_id },
+    templateSelector
+  ) {
+    const cardElement = document
+      .querySelector(templateSelector)
+      .content.querySelector(".elements__card")
+      .cloneNode(true);
+    const cardCountLike = cardElement.querySelector(".elements__count-like");
+    const cardLike = cardElement.querySelector(".elements__like");
+    const cardPhoto = cardElement.querySelector(".elements__photo");
+    const deleteButton = cardElement.querySelector(".elements__delete");
 
-    const cardCountLike = cardElement.querySelector('.elements__count-like');
-    const cardLike = cardElement.querySelector('.elements__like');
-    const cardPhoto = cardElement.querySelector('.elements__photo');
-    const deleteButton = cardElement.querySelector('.elements__delete');
-
-    cardElement.querySelector('.elements__title').textContent = nameValue;
+    cardElement.querySelector(".elements__title").textContent = nameValue;
     cardPhoto.alt = nameValue;
     cardPhoto.src = urlValue;
     cardCountLike.textContent = countLike.length;
-    cardElement.dataset.cardOwnerId =  ownerId;
+    cardElement.dataset.cardOwnerId = ownerId;
     cardElement.dataset.cardId = card_id;
 
-    if(cardElement.dataset.cardOwnerId  !== profileName.dataset.myId )
-    {deleteButton.style.display = 'none'} else{
-      deleteButton.style.display = 'block';
-      deleteButton.addEventListener('click', function () {
+    if (cardElement.dataset.cardOwnerId !== profileName.dataset.myId) {
+      deleteButton.style.display = "none";
+    } else {
+      deleteButton.style.display = "block";
+      deleteButton.addEventListener("click", function () {
         openPopup(popupQuestionDelete);
-        cardIdDelete =  cardElement.dataset.cardId;
+        cardIdDelete = cardElement.dataset.cardId;
       });
     }
 
-    if( countLike.some(function(item){
-        return item._id === profileName.dataset.myId})
-      )
-    {cardLike.classList.add('elements__like_act')}
+    if (
+      countLike.some(function (item) {
+        return item._id === profileName.dataset.myId;
+      })
+    ) {
+      cardLike.classList.add("elements__like_act");
+    }
 
-    cardLike.addEventListener('click', function () {
-      if( cardLike.classList.contains('elements__like_act'))
-        {
+    cardLike.addEventListener("click", function () {
+      if (cardLike.classList.contains("elements__like_act")) {
         disLike(cardElement.dataset.cardId)
-        .then((res)=>{
-          cardCountLike.textContent = res.likes.length;
-          cardLike.classList.remove('elements__like_act');
-        })
-        .catch(err => {
-          showError(err)
-        })
-
+          .then((res) => {
+            cardCountLike.textContent = res.likes.length;
+            cardLike.classList.remove("elements__like_act");
+          })
+          .catch((err) => {
+            showError(err);
+          });
       } else {
         plusLike(cardElement.dataset.cardId)
-        .then((res)=>{
-          cardCountLike.textContent = res.likes.length;
-          cardLike.classList.add('elements__like_act');
-        })
-        .catch(err => {
-          showError(err)
-        })
-       }
-      });
+          .then((res) => {
+            cardCountLike.textContent = res.likes.length;
+            cardLike.classList.add("elements__like_act");
+          })
+          .catch((err) => {
+            showError(err);
+          });
+      }
+    });
 
-      cardPhoto.addEventListener('click', function () {
-        popupPhoto.src = urlValue;
-        popupPhoto.alt = nameValue;
-        subtitlePhoto.textContent = nameValue;
-        openPopup(fullPhoto);
-        });
+    cardPhoto.addEventListener("click", function () {
+      popupPhoto.src = urlValue;
+      popupPhoto.alt = nameValue;
+      subtitlePhoto.textContent = nameValue;
+      openPopup(fullPhoto);
+    });
 
-      return cardElement;
+    return cardElement;
   }
 }
 
@@ -153,6 +162,7 @@ class Card {
 
 //объявить функию для добавления карточки
 export function addCard(nameValue, urlValue, countLike, ownerId, card_id) {
-  cardsContainer.prepend(createCard(nameValue, urlValue, countLike, ownerId, card_id));
+  cardsContainer.prepend(
+    createCard(nameValue, urlValue, countLike, ownerId, card_id)
+  );
 }
-
